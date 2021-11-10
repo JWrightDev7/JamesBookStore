8b0af80b9c3978c80b4394d589785f10e6a1cfee
James Wright <JAMES.WRIGHT01@stclairconnect.ca>
2021-11-10 10:17:24 -0500

    Created ICategoryRepository interface and CategoryRepository class to implement the functions of the interface

---
+++ JamesBooks.DataAccess/CategoryRepository.cs
@@ -0,0 +1,32 @@
+﻿using JamesBooks.DataAccess.Repository;
+using JamesBooks.DataAccess.Repository.IRepository;
+using JamesBooks.Models;
+using JamesBookStore.DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JamesBooks.DataAccess
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Category category)
+        {
+            //uses .net linq to get the first or default value and matches the id to the category id using a generic entity
+            var objFromDb = _db.Categories.FirstOrDefault(s => s.Id == category.Id);
+            if(objFromDb != null)
+            {
+                objFromDb.Name = category.Name;
+                _db.SaveChanges(); //saves changes if the obj is not null
+            }
+        }
+    }
+}
