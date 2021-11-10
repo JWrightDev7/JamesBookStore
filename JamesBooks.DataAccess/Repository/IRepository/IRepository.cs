f3d7a627a21c042ecc2b54ff0ec09a1bf56df468
James Wright <JAMES.WRIGHT01@stclairconnect.ca>
2021-11-10 10:04:59 -0500

    Created IRepository Interface, Created Repository Class to implement the interface to give functionality to the CRUD functions

---
+++ JamesBooks.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JamesBooks.DataAccess.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        T Get(int id);
+
+        IEnumerable<T> GetAll(
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            string includeProperties = null
+            );
+
+        T GetFirstOrDefault(
+            Expression<Func<T, bool>> filter = null,
+            string includeProperties = null
+            );
+
+        void Add(T entity); //used to add an entity
+        void remove(int id); //used to remove an entity
+        void remove(T entity); //another way to remove entities
+        void removeRange(IEnumerable<T> entity); //used to a range of entities
+    }
+}
